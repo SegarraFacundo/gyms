a729d9905aeb96944e9ff0d8beddddb744403255
facundosegarra@outlook.com <facundosegarra@outlook.com>
2023-09-23 10:01:18 -0300

    Agregando el editar cliente

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ function navbarMenu() {
 
   element.classList.remove('hidden');
 }
+
+// Initialization for ES Users
+import {
+  Ripple,
+  Datepicker,
+  Input,
+  Tab,
+  initTE,
+} from "tw-elements";
+
+initTE({ Tab, Ripple, Datepicker, Input });
